dd62eef8d3b2f292f75f2a6fcf9d20b428290067
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-26 22:20:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,71 @@
+def sort_array_asc(array)
+  
+  array.sort 
+  
+end
 
+def sort_array_desc(array)
+  
+  array.sort do |a, b|
+    if a == b 
+      0 
+    elsif a < b 
+      1 
+    elsif a > b 
+      -1 
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  
+  array.sort do |a, b|
+    if a.length == b.length 
+      0 
+    elsif a.length < b.length 
+      -1 
+    elsif a.length > b.length 
+      1 
+    end
+  end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse 
+  
+end
+
+def kesha_maker(array)
+  
+  new_array = []
+  array.each do |element|
+    element[2] = "$"
+    new_array << element
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select do |element| 
+    element.start_with?("a")
+    
+  end
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.collect do |element| 
+    if element == "feet" 
+      element
+    else 
+      element << "s"
+    end
+  end
+end
