b28d5bb41a89cd0a80d0c2402769ee76a93d9c62
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-26 22:13:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,7 +50,7 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  array.select { |element| if element.start_with?("a") }
+  array.select { |element| if element.start_with?("a")}
 end
 
 def sum_array(array)
